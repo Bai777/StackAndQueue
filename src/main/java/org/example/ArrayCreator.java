65110d3467022496d94c563b7c4119d0c4ffe21e
bai_777 <bai_777@mail.ru>
2022-09-21 20:02:23 +0300

    создал ArrayCreator.java

--- src/main/java/org/example/ArrayCreator.java
+++ src/main/java/org/example/ArrayCreator.java
@@ -1,5 +1,13 @@
 package org.example;
 
 public class ArrayCreator {
-
+    public static int[] getArray(int quantity){
+        int[] newArray = new int[quantity];
+        int missingNumberIndex = (int) (Math.random() * quantity);
+        for (int i = 0; i < quantity; i++) {
+            if (i < missingNumberIndex) newArray[i] = i + 1;
+            else newArray[i] = i + 2;
+        }
+        return newArray;
+    }
 }
